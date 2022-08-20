f911f9926eda939695d01ec889b09501b46d4ea3
Mr.night <3162173168@qq.com>
2022-08-20 01:37:50 +0000

    添加函数ShellGetStrCMD
    Signed-off-by: Mr.night <3162173168@qq.com>

--- ShellConsole.h
+++ ShellConsole.h
@@ -4,14 +4,16 @@
 #include "main.h"
 #include "usart.h"
 
+#include "cmd.h"
+
 /* 接收最大长度 */
-#define ShellConsoleMaxSize 		256
+#define ShellConsoleMaxSize 		    256
 
 /* 指令最大长度 */
 #define ShellCmdMaxSize 				16
 
 /* 串口打印接口 */
-#define Print 									PrintfDebug
+#define Print 							PrintfDebug
 
 /* 是否开启回显 */
 #define SHELL_USE_ECHO 					1
@@ -23,10 +25,13 @@ HAL_StatusTypeDef ShellConsoleInit(UART_HandleTypeDef* huart);
 void ShellFunction(void);
 
 /* 添加指令 */
-HAL_StatusTypeDef AddCmd(char *Cmd, char * Describe, void (*Function)());
+HAL_StatusTypeDef ShellAddCmd(char *Cmd, char * Describe, void (*Function)());
 
 /* 删除指令 */
-HAL_StatusTypeDef DelCmd(char *Cmd);
+HAL_StatusTypeDef ShellDelCmd(char *Cmd);
+
+/* 阻塞方式获取下一条输入的指令字符串指针 */
+uint8_t* ShellGetStrCMD(void);
 
 /*  DEMO
 
@@ -43,30 +48,37 @@ void test2()
 void del()
 {
 	PrintfDebug("\r\n 删除测试一！ Delete Test 1!");
-	DelCmd("t1");
+	ShellDelCmd("t1");
 }
 
 void del_help()
 {
 	PrintfDebug("\r\n 删除查询指令！ Delete help!");
-	DelCmd("help");
+	ShellDelCmd("help");
 }
 
 void add()
 {
 	PrintfDebug("\r\n 添加测试一！ add Test 1!");
-	AddCmd("t1", "Printf test 1", test1);//添加测试指令1
+	ShellAddCmd("t1", "Printf test 1", test1);//添加测试指令1
+}
+
+void UartTx(void)
+{
+	uint8_t* Cmd = ShellGetStrCMD();
+	PrintfDebug("\r\nGetStr[%.*s]", (strlen((char*)Cmd) - 1), Cmd);
 }
 
 int main(void)
 {
 	PrintfDebug("Start\r\n");
 	ShellConsoleInit(&huart1);
-	AddCmd("t1", "运行test1程序", test1);
-	AddCmd("t2", "运行test2程序", test2);
-	AddCmd("del", "删除t1指令", del);
-	AddCmd("add", "添加t1指令", add);
-	AddCmd("del help", "删除help指令", del_help);
+	ShellAddCmd("t1", "运行test1程序", test1);
+	ShellAddCmd("t2", "运行test2程序", test2);
+	ShellAddCmd("t3", "运行UartTx程序", UartTx);
+	ShellAddCmd("del", "删除t1指令", del);
+	ShellAddCmd("add", "添加t1指令", add);
+	ShellAddCmd("del help", "删除help指令", del_help);
   while (1)
   {
 		ShellFunction();//需要循环调用，才可以运行
